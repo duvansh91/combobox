16b88e5c3a648857d7ff6a3a299df345e6b407a2
Duvan Sarmiento <jaimeduvan1996@gmail.com>
2019-09-11 23:02:50 -0500

    Logic into component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Paper from '@material-ui/core/Paper';
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-import ComboBox from './components/ComboBox';
-import Avatar from '@material-ui/core/Avatar';
-import Chip from '@material-ui/core/Chip';
-import CloseIcon from '@material-ui/icons/Close';
-
+import ComboBox from './examples/ComboBox';
 
 class App extends Component {
   constructor(props) {
@@ -17,7 +10,7 @@ class App extends Component {
         { key: 1, title: 'Juan Mejias', description: '18', image: 'https://image.flaticon.com/icons/svg/701/701993.svg'},
         { key: 2, title: 'Pedro Almirante', description: '25', image: 'https://image.flaticon.com/icons/svg/701/701993.svg' },
         { key: 3, title: 'Mathew Person', description: '30', image: 'https://image.flaticon.com/icons/svg/701/701993.svg' },
-        { key: 4, title: 'Teresa Albañez', description: '40', image: 'https://image.flaticon.com/icons/svg/702/702016.svg' },
+        { key: 4, title: 'Camila Parra', description: '25', image: 'https://image.flaticon.com/icons/svg/702/702016.svg' },
         { key: 5, title: 'Juan Peralta', description: '23', image: 'https://image.flaticon.com/icons/svg/701/701993.svg' },
         { key: 6, title: 'Leonardo Galves', description: '19', image: 'https://image.flaticon.com/icons/svg/701/701993.svg' },
         { key: 7, title: 'Karen Perez', description: '28', image: 'https://image.flaticon.com/icons/svg/702/702016.svg' },
@@ -29,71 +22,16 @@ class App extends Component {
         { key: 13, title: 'Aris Julio', description: '23', image: 'https://image.flaticon.com/icons/svg/701/701993.svg' },
         { key: 14, title: 'Winston Stand', description: '24', image: 'https://image.flaticon.com/icons/svg/701/701993.svg' }
       ],
-      open: false,
-      selected: [],
-      current: []
+      selected: ""
     }
-  }
-
-  onHandleOpen = () => {
-    this.setState({ open: true });
-  }
-
-  handleClose = () => {
-    this.setState({ open: false });
-  }
-
-  onSelect = (item, closed) => {
-    this.state.selected.push(item);
-    this.setState({ open: closed });
-    this.renderSelect();
-  }
-
-  renderSelect = () => {
-    const { rows, selected } = this.state;
-    if (selected.length > 0) {
-      rows.map(row => {
-        if (row.key === this.state.selected[0]) {
-          this.setState({ current: row });
-        }
-      });
-    }
-  }
-
-  handleDelete = () => {
-    this.setState({ selected: [] });
-    this.setState({ current: [] });
   }
 
   render() {
-    const { rows, open, selected, current } = this.state;
+    const { rows } = this.state;
 
     return (
       <div className="App">
-        <Paper className='main-container'>
-          {selected.length > 0 ? (
-            <Chip
-              className="current"
-              key={current.key}
-              avatar={
-                <Avatar alt="Remy Sharp" src={current.image}>
-                </Avatar>
-              }
-              label={current.title}
-              deleteIcon={
-                <CloseIcon />
-              }
-              onDelete={this.handleDelete}
-            />
-          ) : (
-              <Fab variant="extended" className='add-person' onClick={this.onHandleOpen}>
-                <AddIcon />
-                Add person
-                </Fab>
-            )
-          }
-          <ComboBox data={rows} open={open} onClose={this.handleClose} onSelect={this.onSelect} />
-        </Paper>
+        <ComboBox placeHolder="Agrega un persona" data={rows} />
       </div>
     );
   }
---
+++ src/examples/ComboBox.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField';
+import Modal from '@material-ui/core/Modal';
+import Fab from '@material-ui/core/Fab';
+import CloseIcon from '@material-ui/icons/Close';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import AddIcon from '@material-ui/icons/Add';
+import Chip from '@material-ui/core/Chip';
+
+/*
+You can pass by props:
+
+data = {yourdata}
+placeHolder = "Text to show on the button"
+selected = () = {}
+
+*/
+
+
+class ComboBox extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            rows: this.props.data,
+            open: this.props.open,
+            search: '',
+            filteredRows: [],
+            open: false,
+            selected: [],
+            current: [],
+            placeHolder: this.props.placeHolder
+        }
+    }
+
+    onSelectHandle = (item) => {
+        this.state.selected.push(item);
+        this.setState({ search: '' });
+        this.setState({ open: false });
+    }
+
+    updateSearch = (e) => {
+        this.setState({ search: e.target.value });
+    }
+
+    renderRows = (row) => {
+        return (
+            <ListItem key={row.key} button onClick={() => { this.onSelectHandle(row) }}>
+                <ListItemAvatar>
+                    <Avatar alt="Remy Sharp" src={row.image}>
+                    </Avatar>
+                </ListItemAvatar>
+                <ListItemText primary={row.title} secondary={row.description} />
+            </ListItem>);
+    }
+
+    onHandleOpen = () => {
+        this.setState({ open: true });
+    }
+
+    handleClose = () => {
+        this.setState({ open: false });
+    }
+
+    handleDelete = () => {
+        this.setState({ selected: [] });
+        this.setState({ current: [] });
+    }
+
+    render() {
+
+        const { open, search, rows, selected, current, placeHolder } = this.state;
+
+        const filteredRows = rows.filter(row => {
+            return Object.keys(row).some(key => {
+                if (key !== 'key' && key !== 'image') {
+                    return row[key].toLowerCase().indexOf(search.toLowerCase()) !== -1;
+                }
+            })
+        });
+
+        return (
+            <div>
+                    {selected.length > 0 ? (
+                        <Chip
+                            className="current"
+                            key={selected[0].key}
+                            avatar={
+                                <Avatar alt="Remy Sharp" src={selected[0].image}>
+                                </Avatar>
+                            }
+                            label={selected[0].title}
+                            deleteIcon={
+                                <CloseIcon />
+                            }
+                            onDelete={this.handleDelete}
+                        />
+                    ) : (
+                            <Fab variant="extended" className='add-person' onClick={this.onHandleOpen}>
+                                <AddIcon />
+                                {placeHolder}
+                            </Fab>
+                        )
+                    }
+                    <Modal open={open}>
+                        <Paper className='main-container'>
+                            <TextField
+                                size="small"
+                                className='search-box'
+                                label="Search"
+                                className={'search'}
+                                value={search}
+                                onChange={this.updateSearch}
+                                variant="outlined"
+                            />
+                            <Fab size="small" color="primary" className="close" onClick={this.handleClose}>
+                                <CloseIcon />
+                            </Fab>
+                            <List className="list-rows" style={{ maxHeight: 350 }}>
+                                {
+                                    filteredRows.map(current => {
+                                        return this.renderRows(current);
+                                    })
+                                }
+                            </List>
+                        </Paper>
+                    </Modal>
+            </div>
+        );
+    }
+}
+
+export default ComboBox;
